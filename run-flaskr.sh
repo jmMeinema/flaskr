29d89e79f69d8ab30ba4302244603c6b0ac9cbbf
jonathon Meinema <jonmeinema@gmail.com>
2021-04-26 03:21:43 +0000

    actually run

--- run-flaskr.sh
+++ run-flaskr.sh
@@ -2,4 +2,15 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-[[ $DIR != $PWD ]] && echo "This should only be called from within $DIR" && exit
+[[ $DIR != $PWD ]] && echo "This should only be called from within $DIR" && exit 1
+
+. ./venv/bin/activate
+
+export FLASK_APP=flaskr
+export FLASK_ENVIRONMENT=development
+
+flask run
+
+exit 0
+
+
